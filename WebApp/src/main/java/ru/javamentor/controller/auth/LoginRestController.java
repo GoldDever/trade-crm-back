9bda5987d8f13fcf4d26711c7bf7b620e39fe7e9
Fedor Doronin <fdanbb@gmail.com>
2020-12-18 00:28:32 +0300

    created UserLoginDto, created LoginRestController, connected Model to WebApp.

---
+++ WebApp/src/main/java/ru/javamentor/controller/auth/LoginRestController.java
@@ -0,0 +1,28 @@
+package ru.javamentor.controller.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.javamentor.dto.user.UserLoginDto;
+
+@RestController
+@RequestMapping("api/auth")
+public class LoginRestController {
+    private final UserLoginDto userLoginDto;
+
+    @Autowired
+    public LoginRestController(UserLoginDto userLoginDto) {
+        this.userLoginDto = userLoginDto;
+    }
+
+    @GetMapping("/login")
+    public ResponseEntity<UserLoginDto> loginIntoApp(UserLoginDto userLoginDto) {
+        if (userLoginDto == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
