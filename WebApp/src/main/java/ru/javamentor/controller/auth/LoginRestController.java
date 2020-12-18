c9d291bc7b495984ab8adcf66dd922a420f4b92c
Fedor Doronin <fdanbb@gmail.com>
2020-12-19 01:39:23 +0300

    LoginRestController modified

--- WebApp/src/main/java/ru/javamentor/controller/auth/LoginRestController.java
+++ WebApp/src/main/java/ru/javamentor/controller/auth/LoginRestController.java
@@ -1,9 +1,9 @@
 package ru.javamentor.controller.auth;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.javamentor.dto.user.UserLoginDto;
@@ -11,21 +11,11 @@ import ru.javamentor.dto.user.UserLoginDto;
 @RestController
 @RequestMapping("api/auth")
 public class LoginRestController {
-    private final UserLoginDto userLoginDto;
-
-    @Autowired
-    public LoginRestController(UserLoginDto userLoginDto) {
-        this.userLoginDto = userLoginDto;
-    }
-
     /**
      * Контроллер для логина
      */
     @GetMapping("/login")
-    public ResponseEntity<UserLoginDto> loginIntoApp(UserLoginDto userLoginDto) {
-        if (userLoginDto == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    public ResponseEntity<String> loginIntoApp(@RequestBody UserLoginDto userLoginDto) {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
