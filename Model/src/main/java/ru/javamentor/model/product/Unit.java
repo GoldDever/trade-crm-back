2647e46e757431e31ac2896a591decc213aab732
TATYANA SEMENTKOVSKAYA <sharapovatv@gmail.com>
2020-12-19 01:16:22 +0300

    Entity product

---
+++ Model/src/main/java/ru/javamentor/model/product/Unit.java
@@ -0,0 +1,40 @@
+package ru.javamentor.model.product;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "units")
+public class Unit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "unit_name")
+    private String unitName;
+
+    public Unit() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUnitName() {
+        return unitName;
+    }
+
+    public void setUnitName(String unitName) {
+        this.unitName = unitName;
+    }
+}
+
