5a58d741686485d848fafd16e30e4b224369920c
Fedor Doronin <fdanbb@gmail.com>
2020-12-29 19:11:33 +0300

    changed to ManyToOne

--- Model/src/main/java/ru/javamentor/model/OrderApprove.java
+++ Model/src/main/java/ru/javamentor/model/OrderApprove.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -23,7 +24,7 @@ public class OrderApprove {
     @Column(name = "text")
     private String text;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "order_id")
     private Order order;
 
