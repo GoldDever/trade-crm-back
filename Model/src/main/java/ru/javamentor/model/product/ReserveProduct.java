c8f4e9288de59cdc9b723eba1f42e8efb97f2d54
kurma <kurmajavamentor@gmail.com>
2021-01-08 15:21:20 +0300

    Изменены связи

--- Model/src/main/java/ru/javamentor/model/product/ReserveProduct.java
+++ Model/src/main/java/ru/javamentor/model/product/ReserveProduct.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -19,11 +20,11 @@ public class ReserveProduct {
     @Column(name = "id")
     private Long id;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "product_id")
     private Product product;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "order_id")
     private Order order;
 
