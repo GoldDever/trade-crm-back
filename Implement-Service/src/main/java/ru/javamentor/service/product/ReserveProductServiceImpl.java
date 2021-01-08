ce3c09cf91078f1ce5cabb8e8884d8b8da9e6d7d
Seylem <dmitriy-ne@bk.ru>
2021-01-08 13:11:38 +0300

    исправил замечания 2.0

--- Implement-Service/src/main/java/ru/javamentor/service/product/ReserveProductServiceImpl.java
+++ Implement-Service/src/main/java/ru/javamentor/service/product/ReserveProductServiceImpl.java
@@ -22,6 +22,6 @@ public class ReserveProductServiceImpl implements ReserveProductService {
     @Transactional
     @Override
     public void removeOrderReserve(Long orderId) {
-        reserveProductRepository.deleteByOrderId(Long.valueOf(orderId));
+        reserveProductRepository.deleteByOrderId(orderId);
     }
 }
