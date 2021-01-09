f90145828b79bddde54512a8f2c2c6c9178c70cc
temirlan100 <temirlan1990@gmail.com>
2021-01-09 22:26:32 +0600

    Fix: add init datas and modify query

--- Repository/src/main/java/ru/javamentor/repository/product/ReserveProductRepository.java
+++ Repository/src/main/java/ru/javamentor/repository/product/ReserveProductRepository.java
@@ -1,6 +1,7 @@
 package ru.javamentor.repository.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import ru.javamentor.model.product.ReserveProduct;
@@ -9,15 +10,17 @@ public interface ReserveProductRepository extends JpaRepository<ReserveProduct, 
     void deleteByOrderId(Long id);
 
     @Query("SELECT rp.productCount FROM ReserveProduct rp " +
-            "WHERE rp.product = :productId AND rp.order = :orderId")
+            "WHERE rp.product.id = :productId AND rp.order.id = :orderId")
     Integer getReserveProductCount(@Param("orderId") Long orderId, @Param("productId") Long productId);
 
+    @Modifying
     @Query("DELETE FROM ReserveProduct rp " +
-            "WHERE rp.product = :productId AND rp.order = :orderId")
-    void deleteReserve(Long orderId, Long productId);
+            "WHERE rp.product.id = :productId AND rp.order.id = :orderId")
+    void deleteReserve(@Param("orderId") Long orderId, @Param("productId") Long productId);
 
+    @Modifying
     @Query("UPDATE ReserveProduct rp SET rp.productCount=rp.productCount - :reserveProductCountEdit " +
-            "WHERE rp.product = :productId AND rp.order = :orderId")
+            "WHERE rp.product.id = :productId AND rp.order.id = :orderId")
     void updateReserveProductCount(@Param("reserveProductCountEdit") Integer reserveProductCountEdit,
                                    @Param("orderId") Long orderId,
                                    @Param("productId") Long productId);
