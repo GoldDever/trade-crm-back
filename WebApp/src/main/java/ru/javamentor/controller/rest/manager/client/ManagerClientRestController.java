cef3bac30f02e9d3784b287bc3df3bebc64780fc
kurma <kurmajavamentor@gmail.com>
2021-02-04 23:55:23 +0300

    Добавлены методы в ManagerClientRestController

--- WebApp/src/main/java/ru/javamentor/controller/rest/manager/client/ManagerClientRestController.java
+++ WebApp/src/main/java/ru/javamentor/controller/rest/manager/client/ManagerClientRestController.java
@@ -1,11 +1,31 @@
 package ru.javamentor.controller.rest.manager.client;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.javamentor.dto.order.ClientDto;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/manager/client")
 public class ManagerClientRestController {
 
+    @GetMapping()
+    ResponseEntity<List<ClientDto>> getClientDtoListByManagerId() {
+        //TODO реализовать. Метод возвращает список клиентов прикрепленных к текущему менеджеру. Добавляем лог.
+        return null;
+    }
+
+    @GetMapping("/{clientId}")
+    ResponseEntity<ClientDto> getClientDtoByClientId(@PathVariable Long clientId) {
+        //TODO реализовать. Метод возвращает  клиента по id,
+        // Если клиент не привязан к менеджеру, возвращаем ответ "Вы не можете просматривать данного клиента."
+        // Добавить проверку на существование клиента по id.
+        // Если клиента не существует, возвращаем "Клиент с id={clientId}, не найден." Добавляем лог
+        return null;
+    }
 
 }
