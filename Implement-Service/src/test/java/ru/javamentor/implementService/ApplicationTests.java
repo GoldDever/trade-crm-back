d280484a791e8689f0f20800c84a2944658a3a32
Антон <avdev1987@gmail.com>
2021-02-16 22:43:10 +0500

    Замечания устранены

--- Implement-Service/src/test/java/ru/javamentor/implementService/ApplicationTests.java
+++ Implement-Service/src/test/java/ru/javamentor/implementService/ApplicationTests.java
@@ -1,4 +1,24 @@
 package ru.javamentor.implementService;
 
-public class ApplicationTests {
+import org.junit.jupiter.api.Test;
+import ru.javamentor.service.client.ClientService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class ApplicationTests  {
+
+
+    private ClientService clientService;
+
+    public ApplicationTests(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @Test
+    public void isExistsByClientIdTest() throws Exception {
+        assertTrue(true, clientService.isExistsByClientId(1l));
+
+    }
 }
