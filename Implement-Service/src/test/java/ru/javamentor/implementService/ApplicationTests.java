439abdb62869b641aaa6fd53a9cd706c2814f730
Антон <avdev1987@gmail.com>
2021-02-17 18:29:53 +0500

    Первый тест

--- Implement-Service/src/test/java/ru/javamentor/implementService/ApplicationTests.java
+++ Implement-Service/src/test/java/ru/javamentor/implementService/ApplicationTests.java
@@ -1,24 +1,47 @@
 package ru.javamentor.implementService;
-
 import org.junit.jupiter.api.Test;
-import ru.javamentor.service.client.ClientService;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.javamentor.model.user.Client;
+import ru.javamentor.repository.user.ClientRepository;
+import ru.javamentor.service.client.ClientService;
+import ru.javamentor.service.client.ClientServiceImpl;
+
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import static org.mockito.Mockito.when;
 
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ApplicationTests  {
+    
+    @Autowired
+    @InjectMocks
+    private ClientServiceImpl clientService;
 
 
-    private ClientService clientService;
-
-    public ApplicationTests(ClientService clientService) {
-        this.clientService = clientService;
-    }
+    @MockBean
+    private ClientRepository clientRepository;
+    Client client=new Client();
 
     @Test
-    public void isExistsByClientIdTest() throws Exception {
-        assertTrue(true, clientService.isExistsByClientId(1l));
+    public void isExistsByClientIdTest()  {
+
+        client.setId(10L);
+        Mockito.when(clientRepository.existsById(10L)).thenReturn(true);
+
+        assertTrue(clientService.isExistsByClientId(10L));
 
     }
 }
