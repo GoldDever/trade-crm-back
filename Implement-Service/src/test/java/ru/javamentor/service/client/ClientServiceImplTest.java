2f0810b76aea30ed9b97ddf3733b5d3404135cf3
Антон <avdev1987@gmail.com>
2021-02-18 15:24:50 +0500

    Первый тест 2

---
+++ Implement-Service/src/test/java/ru/javamentor/service/client/ClientServiceImplTest.java
@@ -0,0 +1,36 @@
+package ru.javamentor.service.client;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import ru.javamentor.repository.user.ClientRepository;
+
+import static org.junit.Assert.*;
+
+public class ClientServiceImplTest {
+
+@Mock
+ClientRepository clientRepository;
+
+private ClientServiceImpl
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void isExistsByClientId() {
+    }
+
+    @Test
+    public void getClientDtoListFromClientsWithManager() {
+    }
+
+    @Test
+    public void getClientDtoByClientId() {
+    }
+
+    @Test
+    public void relationClientWithManager() {
+    }
+}
